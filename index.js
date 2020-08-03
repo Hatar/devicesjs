30458a2a9eec9f0800f08e8943da6ac6020256d5
Amine <amine.hatar@gmail.com>
2020-08-03 11:55:36 +0100

    create Custom toolbar with deffirent type Marker

--- index.js
+++ index.js
@@ -2,21 +2,33 @@ var express = require('express')
 var socket = require('socket.io')
 var application = express();
 
-var server = application.listen(5000,function(){
-   console.log("server run")
+var server = application.listen(5000, function () {
+	console.log("server run")
 })
-
 
 application.use(express.static('public'))
 
 var sio = socket(server)
 
-sio.on('connection',function(visiteur){
-   console.log('we have a new user id => ',visiteur.id)
-   let devices = {}
-   visiteur.on('device',function(_device){
-      console.log(_device)
-      devices[_device.id] = _device;
-      sio.sockets.emit('data_devices',_device)
-    })
- })
+sio.on('connection', function (visiteur) {
+	console.log('we have a new user id => ', visiteur.id)
+	let devices = {},
+		lines = {}
+	visiteur.on('device', function (_device) {
+		console.log(_device)
+		devices[_device.id] = _device;
+		sio.sockets.emit('data_devices', _device)
+	})
+
+	visiteur.on('line', function (_line) {
+		console.log(_line)
+		lines[_line.id] = _line;
+		sio.sockets.emit('data_lines', _line)
+	})
+
+	visiteur.on('detecter', function (_detect) {
+		console.log(_detect)
+		lines[_detect.id] = _detect;
+		sio.sockets.emit('data_Detect', _detect)
+	})
+})
--- public/js/main.js
+++ public/js/main.js
@@ -1,119 +1,133 @@
 //Variable
 var socket = io.connect('http://localhost:5000'),
-	data = {"devices":[],"lines" :[],"detecteur":[]},
+	data = {
+		"devices": [],
+		"lines": [],
+		"capteurs": []
+	},
 	fname = document.querySelector("#fname"),
 	nameL = document.querySelector("#nameL"),
 	nameD = document.querySelector("#nameD"),
 	SubmitFormDev = document.querySelector('#submitFormDevice'),
 	SubmitFormLin = document.querySelector('#submitFormLines'),
-	SubmitFormDect = document.querySelector('#submitFormDtect'),
+	SubmitFormCap = document.querySelector('#submitFormCap'),
 	save = document.querySelector('#savedevice'),
-	btnClose = document.querySelectorAll('#close'),
 	deviceModal = document.querySelector('#device'),
 	list_icones = document.querySelector('#icones'),
 	colors = ['#1E90FF', '#FF1493'],
 	selectedColor,
 	customColor,
-	colorButtons ={},
+	colorButtons = {},
+	polyline ="",
 	x = 0,
 	y = 0,
-	x1 =[],
-	y1 =[],
-	x2 =[],
-	y2 =[],
+	x1 = [],
+	y1 = [],
+	x2 = [],
+	y2 = [],
 	modalId = 0,
 	map = L.map('map').setView([31.58831, -7.11138], 6);
 	L.tileLayer('https://via.placeholder.com/{z}/{x}/{y}.png').addTo(map);
 
-	clearBtn.onclick = (e)=>{
-		if(id_layer.value) drawnItems._layers[id_layer.value]._icon.src = document.querySelector("#image_1").src
-	};
+	//change Image Device By Socket
+	socket.on('data_devices', function (result) {
+		clearBtn.onclick = (e) => {
+			let res_device = result.filter(res => res.id === id_layer.value)
+			let _id = res_device[0].id
+			id_layer.value !== _id ? drawnItems._layers[id_layer.value]._icon.src : drawnItems._layers[id_layer.value]._icon.src = document.querySelector("#image_1").src
+		};
+		useBtn.onclick = (e) => {
+			let res_device = result.filter(res => res.id === id_layer.value)
+			let _id = res_device[0].id
+			id_layer.value !== _id ? drawnItems._layers[id_layer.value]._icon.src : drawnItems._layers[id_layer.value]._icon.src = document.querySelector("#image_2").src
+		};
+		warBtn.onclick = (e) => {
+			let res_device = result.filter(res => res.id === id_layer.value)
+			let _id = res_device[0].id
+			id_layer.value !== _id ? drawnItems._layers[id_layer.value]._icon.src : drawnItems._layers[id_layer.value]._icon.src = document.querySelector("#image_3").src
+		};
+	})
 
-	useBtn.onclick =  (e)=>{
-		if(id_layer.value) drawnItems._layers[id_layer.value]._icon.src = document.querySelector("#image_2").src
-	};
+//Initial Map
+var drawnItems = new L.FeatureGroup();
+map.addLayer(drawnItems);
+var drawControl = new L.Control.Draw({
+	edit: {
+		featureGroup: drawnItems,
+		remove: false
+	}
+});//End Draw Control
 
-	warBtn.onclick =  (e)=>{
-		if(id_layer.value) drawnItems._layers[id_layer.value]._icon.src = document.querySelector("#image_3").src
-	};
+var customMarker = L.Icon.extend({
+	options: {
+		iconSize: new L.Point(30, 30),
+		iconUrl: "https://img.icons8.com/dusk/64/000000/garage--v1.png",
+	}
+});
 
-	var drawnItems = new L.FeatureGroup();
-	map.addLayer(drawnItems);
-	var drawControl = new L.Control.Draw({
-		edit: {
-			featureGroup: drawnItems,
-		},
-		rectangle :{
+L.Draw.Capteur = L.Draw.Marker.extend({
+	statics : {
+		TYPE :'capteur'
+	},
+	options: {
+		icon: new customMarker(),
+		repeatMode: false,
+		zIndexOffset: 2000 // This should be > than the highest z-index any markers
+	},
+	initialize: function (map, options) {
+		// Save the type so super can fire, need to do this as cannot do this.TYPE :(
+		this.type = L.Draw.Capteur.TYPE;
+
+		L.Draw.Feature.prototype.initialize.call(this, map, options);
+	}
+})//End Draw Capteur
+
+
+
+//Custom Icon Toolbar
+L.DrawToolbar.prototype.getModeHandlers = function(map){
+	return [{
+		enabled: true,
+		handler: new L.Draw.Rectangle(map, {
 			allowIntersection: false,
 			showArea: true,
 			metric: true,
 			repeatMode: true,
-			selectedPathOptions: {
-        maintainColor: true,
-        opacity: 0.3
-    },
-		},
-	});
-
-//Adding Toolbar Leaflet
-L.DrawToolbar.include({
-	getModeHandlers: function (map) {
-		return [
-			{
-				enabled: true,
-				handler: new L.Draw.Rectangle(map, {
-					allowIntersection: false,
-					showArea: true,
-					metric: true,
-					repeatMode: true,
-					shapeOptions: {
-						color:"#F2F2F2",
-						weight: 5,
-						fillOpacity: .5
-					}
-				}),
-				title: 'Add Line ',
-			},
-			{
-				enabled: true,
-				handler: new L.Draw.Marker(map, {
-					icon: new L.Icon.Default()
-				}),
-				title: 'Add Device'
-			},
-			{
-				enabled: true,
-				handler: new L.Draw.Circle(map, {
-					allowIntersection: false,
-					showArea: true,
-					metric: true,
-					repeatMode: true,
-					shapeOptions: {
-						color:"#F2F2F2",
-						weight: 5,
-						fillOpacity: .5
-					}
-				}),
-				title: 'Add Detecteur',
+			shapeOptions: {
+				color: "#F2F2F2",
+				weight: 5,
+				fillOpacity: .5
 			}
-		];
+		}),
+		title: 'Add Line ',
+	},
+	{
+		enabled: true,
+		handler: new L.Draw.Marker(map, { icon: new L.Icon.Default() }),
+		title: 'Add Device'
+	},
+	{
+		enabled: true,
+		handler: new L.Draw.Capteur(map, this.options.capteur),
+		title: 'Add Capteur',
 	}
-});
+	];
+};
 map.addControl(drawControl);
 
-//Submit Form Device
+
+//Submit Form [Device,Line,Detecteur]
 SubmitFormDev.addEventListener('submit', function (e) {
 	e.preventDefault();
-    let result = data.devices.filter(marker => marker.id === modalId);
-		if (result.length == 0) {
+	let result = data.devices.filter(marker => marker.id === modalId);
+	if (result.length == 0) {
 		data.devices.push({
 			id: modalId,
 			fname: fname.value,
 			x: x,
 			y: y
 		});
-		//Send Data to Serveur
-		socket.emit('device',data.devices)
+		socket.emit('device', data.devices)
 		SubmitFormDev.reset();
 		$('#device').modal('toggle');
 	} else {
@@ -122,7 +136,6 @@ SubmitFormDev.addEventListener('submit', function (e) {
 		$('#device').modal('toggle');
 	}
 })
-//Submit Form Line
 SubmitFormLin.addEventListener('submit', function (e) {
 	e.preventDefault();
 	let result = data.lines.filter(lines => lines.id === modalId);
@@ -135,123 +148,129 @@ SubmitFormLin.addEventListener('submit', function (e) {
 			x2: x2,
 			y2: y2,
 		});
+		socket.emit('line', data.lines)
 		SubmitFormLin.reset();
 		$('#lines').modal('toggle');
 	} else {
 		result[0].nameL = nameL.value;
 		SubmitFormLin.reset();
 		$('#lines').modal('toggle');
-		changeColor()
 	}
-	changeColor()
 })
-//Submit Form Detectteur
-SubmitFormDect.addEventListener('submit', function (e) {
+SubmitFormCap.addEventListener('submit', function (e) {
 	e.preventDefault();
-	let result = data.detecteur.filter(detect => detect.id === modalId);
+	let result = data.capteurs.filter(capteur => capteur.id === modalId);
 	if (result.length == 0) {
-	data.detecteur.push({
-		id: modalId,
-		nameD: nameD.value,
-		x: x,
-		y : y
-	} ) ;
-
-
-	SubmitFormDect.reset();
-	$('#detecteur').modal('toggle');
+		data.capteurs.push({
+			id: modalId,
+			nameC: nameC.value,
+			x: x,
+			y: y
+		});
+		socket.emit('capteur', data.capteurs)
+		SubmitFormCap.reset();
+		$('#capteur').modal('toggle');
 	} else {
-		result[0].nameD = nameD.value;
-		SubmitFormDect.reset();
-		$('#detecteur').modal('toggle');
+		result[0].nameC = nameC.value;
+		SubmitFormCap.reset();
+		$('#capteur').modal('toggle');
 	}
 })
-
-//Event Draw Create Shape Leaflet
-map.on(L.Draw.Event.CREATED,function (event) {
-
-	var  type = event.layerType,
-			 layer = event.layer;
-			 drawnItems.addLayer(layer);
-			 modalId = layer._leaflet_id
-
-			 if (type === 'marker') {
+map.on(L.Draw.Event.CREATED, function (event) {
+	var thisLayer,
+			getIdClick;
+	var type = event.layerType,
+			layer = event.layer;
+			drawnItems.addLayer(layer),
+			modalId = layer._leaflet_id
+			if (type === 'rectangle') {
+				$("#lines").modal();
+				$('#color-palette').css('display', 'none')
+					x1 = layer._latlngs[0][0],
+					y1 = layer._latlngs[0][1],
+					x2 = layer._latlngs[0][2],
+					y2 = layer._latlngs[0][3],
+					socket.on('data_lines',function(resLines){
+						layer.addEventListener('click', function (e) {
+							getIdClick =e.target._leaflet_id
+							let result = resLines.filter(resL => resL.id == getIdClick);
+							if (result.length != 0) {
+								nameL.value = result[0].nameL;
+							}
+						})//End Layer Clicked
+						$(document).on("click","path.leaflet-interactive", function(e){
+							thisLayer = $(this);
+							$('#color-palette').css('display', 'block')
+							$('#lines').modal('show');
+						})
+						$(document).on('click','#color-palette .color-button',function(){
+							var colorSelected = $(this).css("backgroundColor");
+							changeColor(thisLayer, colorSelected)
+						})
+					})
+					closeL.addEventListener('click', function (e) {
+						if (nameL.value === '') {
+							map.eachLayer(function (layer) {
+								map.removeLayer(drawnItems._layers[modalId])
+							})
+						}
+					})
+			}
+			if (type === 'marker') {
 				SubmitFormDev.reset();
-					console.log(layer._leaflet_id)
-				  id_layer.value = layer._leaflet_id
+					id_layer.value = layer._leaflet_id
 					x = layer._latlng.lat,
 					y = layer._latlng.lng
-					$("#device").modal();
-					layer.addEventListener('click', function (e) {
-						modalId = e.target._leaflet_id
-						$('#device').modal('show');
-						let result = data.devices.filter(device => device.id == e.target._leaflet_id);
-						if (result.length !== 0) {
-							fname.value = result[0].fname;
-						}
-						$('#device').modal('toggle');
-				})
-			}
-			if(type === 'rectangle'){
-				$("#lines").modal();
-				x1 = layer._latlngs[0][0],
-				y1 = layer._latlngs[0][1],
-				x2 = layer._latlngs[0][2],
-				y2 = layer._latlngs[0][3]
-				$("#lines").modal();
+				$("#device").modal();
 				layer.addEventListener('click', function (e) {
-				$('#color-palette').css('display','none')
-				modalId = e.target._leaflet_id
-				$('#lines').modal('show');
-				let result = data.lines.filter(lines => lines.id === e.target._leaflet_id);
-				if (result.length != 0) {
-					nameL.value = result[0].nameL;
-				}
-			})
-			}
-			if(type ==='circle'){
-
-				$("#detecteur").modal();
-				SubmitFormDect.reset();
-					x = layer._latlng.lat,
-					y = layer._latlng.lng
-					$("#detecteur").modal();
-					layer.addEventListener('click', function (e) {
-						modalId = e.target._leaflet_id
-						$('#detecteur').modal('show');
-						let result = data.detecteur.filter(detect => detect.id == e.target._leaflet_id);
-						if (result.length !== 0) {
-							nameD.value = result[0].nameD;
-						}
-						$('#detecteur').modal('toggle');
-				} )
-			}
-			for(var i=0;i<btnClose.length;i++){
-				btnClose[i].addEventListener('click',function(e){
-					if(type === 'marker' && fname.value === '') {
-						map.eachLayer(function(layer){
-							map.removeLayer(drawnItems._layers[modalId])
-						})
-					}else if(type === 'rectangle' && nameL.value === '') {
-						map.eachLayer(function(layer){
-							map.removeLayer(drawnItems._layers[modalId])
-						})
-					}else if(type === 'circle' && nameD.value === ''){
-						map.eachLayer(function(layer){
+					id_layer.value = modalId = e.target._leaflet_id
+					$('#device').modal('show');
+					let result = data.devices.filter(device => device.id == e.target._leaflet_id);
+					if (result.length !== 0) {
+						fname.value = result[0].fname;
+					}
+					$('#device').modal('toggle');
+				})
+				closeD.addEventListener('click', function (e) {
+					if (fname.value === '') {
+						map.eachLayer(function (layer) {
 							map.removeLayer(drawnItems._layers[modalId])
 						})
 					}
-			})
+				})
+			}
+			if (type === 'capteur') {
+					SubmitFormCap.reset()
+					id_layer.value = layer._leaflet_id
+					x = layer._latlng.lat,
+					y = layer._latlng.lng
+				$("#capteur").modal();
+				layer.addEventListener('click', function (e) {
+					id_layer.value = modalId = e.target._leaflet_id
+					$('#capteur').modal('show');
+					let result = data.capteurs.filter(capteur => capteur.id == e.target._leaflet_id);
+					if (result.length !== 0) {
+						nameC.value = result[0].nameC;
+					}
+					$('#capteur').modal('toggle');
+				})
+				closeC.addEventListener('click', function (e) {
+					if (nameC.value === '') {
+						map.eachLayer(function (layer) {
+							map.removeLayer(drawnItems._layers[modalId])
+						})
+					}
+				})
 			}
 });
 
-//Event Draw Update Shape Leaflet
-map.on(L.Draw.Event.EDITED, function(e) {
+map.on(L.Draw.Event.EDITED, function (e) {
 	var layers = e.layers;
-	layers.eachLayer(function(layer) {
+
+	layers.eachLayer(function (layer) {
 		var LineId = L.stamp(layer);
-		if(layers.toGeoJSON().features[0].geometry.type === 'Polygon'){
-			let coordinates =layers.toGeoJSON().features[0].geometry.coordinates
+		if (layers.toGeoJSON().features[0].geometry.type === 'Polygon') {
+			let coordinates = layers.toGeoJSON().features[0].geometry.coordinates
 			for (var i in data.lines) {
 				if (LineId == data.lines[i].id) {
 					data.lines[i].x1 = coordinates[0][0]
@@ -259,61 +278,61 @@ map.on(L.Draw.Event.EDITED, function(e) {
 					data.lines[i].x2 = coordinates[0][2]
 					data.lines[i].y2 = coordinates[0][3]
 				}
-				}
-	}
-	})
-		values = drawnItems._layers;
-		for (var key in values) {
-			for (var i in data.devices) {
-				if (key == data.devices[i].id) {
-					data.devices[i].x = values[key]._latlng.lat
-					data.devices[i].y = values[key]._latlng.lng
-				}
 			}
 		}
-		values_dect = drawnItems._layers;
-		for (var key in values_dect) {
-			for (var i in data.detecteur) {
-				if (key == data.detecteur[i].id) {
-					data.detecteur[i].x = values_dect[key]._latlng.lat
-					data.detecteur[i].y = values_dect[key]._latlng.lng
-				}
-			}
-		}});
+	})
 
-//Get Data
+	values = drawnItems._layers;
+	for (var key in values) {
+		for (var i in data.devices) {
+			if (key == data.devices[i].id) {
+				data.devices[i].x = values[key]._latlng.lat
+				data.devices[i].y = values[key]._latlng.lng
+			}
+		}
+	}
+	for (var key in values) {
+		for (var i in data.capteurs) {
+			if (key == data.capteurs[i].id) {
+				data.capteurs[i].x = values[key]._latlng.lat
+				data.capteurs[i].y = values[key]._latlng.lng
+			}
+		}
+	}
+});
+
+//Get Data & Print if from html
 document.querySelector('#data').addEventListener('click', function () {
 	document.querySelector('.line').innerHTML = JSON.stringify(data)
 })
 
-//Function Show icon Device[clear,use,warning] file upload
-function ShowIcon(input, selector){
-	if (input.files && input.files[0])
-	{
+//Show Icon from Marker Created
+function ShowIcon(input, selector) {
+	if (input.files && input.files[0]) {
 		var reader = new FileReader();
 		reader.onload = function (e) {
-		$('#'+selector).attr('src', e.target.result)
-		 };
+			$('#' + selector).attr('src', e.target.result)
+		};
 		reader.readAsDataURL(input.files[0]);
 	}
 }
-/*
-Start Change Color rectangle when Created
-*/
 
-function selectColor(color){
-	selectedColor=color;
-	for(var i=0;i<colors.length;i++){
+function selectColor(color) {
+	selectedColor = color;
+	for (var i = 0; i < colors.length; i++) {
 		let currentColor = colors[i]
-		colorButtons[currentColor].style.border =currentColor == color ? '2px solid #333' : '2px solid #fff';
+		colorButtons[currentColor].style.border = currentColor == color ? '2px solid #333' : '2px solid #fff';
 	}
 }
+
 buildColorPalette();
+
 function buildColorPalette() {
 	var colorPalette = document.getElementById('color-palette');
 	for (var i = 0; i < colors.length; ++i) {
 		var currColor = colors[i];
 		var colorButton = makeColorButton(currColor);
+		//Lines
 		colorPalette.appendChild(colorButton);
 		colorButtons[currColor] = colorButton;
 	}
@@ -324,24 +343,16 @@ function makeColorButton(color) {
 	var button = document.createElement('span');
 	button.className = 'color-button';
 	button.style.backgroundColor = color;
-	L.DomEvent.addListener(button, 'click', function(e) {
-		drawnItems._layers[modalId].options.color = color;
-		customColor=color
+
+	L.DomEvent.addListener(button, 'click', function (e) {
 		selectColor(color);
 	});
 	return button;
 }
 
-function changeColor(){
-	var n=0;
-	n = $('.leaflet-interactive').length;
-	$(".leaflet-interactive:nth-child("+n+")").css({fill:customColor,stroke:customColor})
+function changeColor(element, colorSelected) {
+	element.attr({
+		fill: colorSelected,
+		stroke: colorSelected
+	})
 }
-
-/*
-	End Change Color rectangle when Created
-*/
-//Get Data Ffrom Server
-socket.on('data_devices',function(result){
-		console.log(result)
-})
